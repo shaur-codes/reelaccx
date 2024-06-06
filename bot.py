ade5299c5a81ee53751e8e022c55e016075829c2
Shaurya Mishra <168108708+shaur-codes@users.noreply.github.com>
2024-06-06 10:07:12 +0530

    added two functions
    loguru and is_connected() for logging and checking if the bot is connnected to internet

--- bot.py
+++ bot.py
@@ -1,74 +1,88 @@
 import discord
-from core import *
 import os
 import asyncio
-import os
+import requests
 from dotenv import load_dotenv
+from core import *
+from loguru import logger
+from colorama import Fore, Style, init
 
 
+init(autoreset=True)
+load_dotenv()
+TOKEN = os.getenv("KEY")
 
-TOKEN=os.getenv("KEY")
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-load_dotenv()
-#VARS
-#accounts=['losttemplates','wtf_su2']
+pre = f"{Fore.BLUE}[BOT]{Style.RESET_ALL}"
+logger.add("bot.log", format="{time} {level} {message}", level="INFO")
+
 
 async def upload_video(channel_id: int, video_file: str) -> bool:
     try:
         channel = await client.fetch_channel(channel_id)
         with open(video_file, 'rb') as file:
             await channel.send(file=discord.File(file))
-        print(f"Video '{video_file}' uploaded to {channel.mention}")
+        logger.info(f"{pre} Video '{video_file}' uploaded to {channel.mention}")
         return True
-    except discord.NotFound or Exception:
-        print("Channel not found. Make sure the channel ID is correct.")
+    except discord.NotFound:
+        logger.error(f"{pre} Channel not found. Make sure the channel ID is correct.")
+        return False
+    except Exception as e:
+        logger.error(f"{pre} Error uploading video: {e}")
         return False
 
-# Example usage:
 
-#1246055019594121226
+async def is_connected():
+    url = 'https://google.com'
+    logger.info(f"{pre} Checking the connectivity of internet...")
+    try:
+        requests.get(url)
+        logger.info(f"{pre} We are up and running")
+    except Exception as e:
+        logger.warning(f"{pre} We are not connected to internet... Trying again in 30 seconds")
+        await asyncio.sleep(30)
+        await is_connected()
+
 
 async def main():
     await update_account_records(query=None)
     data = loadx()
 
-    #check_post=check_for_new_post()
     for account in data["accounts"]:
-        account_name=account["name"]
+        account_name = account["name"]
         check_for_post = await check_for_new_post(account_name)
         if check_for_post:
-            latest_post=get_latest_post(account_name)
-            if latest_post!=False:
-                post_url=await get_post_url(latest_post)
-                file=f"{latest_post}.mp4"
-                dp= await dump_post(url=post_url,filename=file)
+            latest_post = get_latest_post(account_name)
+            if latest_post:
+                post_url = await get_post_url(latest_post)
+                file = f"{latest_post}.mp4"
+                dp = await dump_post(url=post_url, filename=file)
                 if dp:
                     for server in account['server']:
-                        server_name=server['name']
-                        channel=server['channel']
-                        load_video= await upload_video(channel_id=channel,video_file=file)
-                        print(f"upload has been successful for {server_name}")
-                    print(f"deleting file {file}")
+                        server_name = server['name']
+                        channel = server['channel']
+                        load_video = await upload_video(channel_id=channel, video_file=file)
+                        if load_video:
+                            logger.info(f"{pre} Upload has been successful for {server_name}")
+                    logger.info(f"{pre} Deleting file {file}")
                     os.remove(file)
                 else:
-                    print("E dump_post false")
+                    logger.error(f"{pre} Error in -> dump_post ~ false")
             else:
-                print("E latest post False")
+                logger.info(f"{pre} No new post found for account {account_name}")
         else:
-            print("E no new post")
-        
-
-
+            logger.info(f"{pre} No new post to check for account {account_name}")
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    logger.info(f"{pre} We have logged in as {client.user}")
+    await is_connected()
     while True:
         await main()
-        print("waiting for 1 hour")
-        await asyncio.sleep(3600)  # Call the main function here
-
+        logger.info(f"{pre} Waiting for 1 hour")
+        await asyncio.sleep(3600)
 
 client.run(TOKEN)
