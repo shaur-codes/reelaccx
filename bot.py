cc71e69679ea2362fcc41abbce73a4ab0abcd0e6
Shaurya Mishra <168108708+shaur-codes@users.noreply.github.com>
2024-06-08 18:45:48 +0530

    Update bot.py
    Added more Exception handling

--- bot.py
+++ bot.py
@@ -2,30 +2,29 @@ import discord
 from discord.ext import commands
 import os
 import asyncio
-import requests
+import aiohttp
 from dotenv import load_dotenv
 from core import *
 from loguru import logger
 from colorama import Fore, Style, init
 
 
-
+#vars
 init(autoreset=True)
 load_dotenv()
 TOKEN = os.getenv("KEY")
 VERIFIED_MEMBER_ID=os.getenv("MEMBER_ID")
-
 intents = discord.Intents.default()
 intents.message_content = True
 pre = f"{Fore.BLUE}[BOT]{Style.RESET_ALL}"
 logger.add("bot.log", format="{time} {level} {message}", level="INFO")
-bot = commands.Bot(command_prefix="/",intents=intents)
+# bot = commands.Bot(command_prefix="/",intents=intents)
+bot = commands.AutoShardedBot(command_prefix="/", intents=intents)
 
 
-#COMMANDS
-
+#COMMAND
 @bot.command(name="adduser", description="add an IG username to get reels from ")
-async def adduser(ctx, username: str, member_id: int):
+async def adduser(ctx, username: str, member_id: str):
     if member_id == VERIFIED_MEMBER_ID:
         await add_new_account(username)
         await ctx.send(f"User {username} added successfully!")
@@ -33,7 +32,7 @@ async def adduser(ctx, username: str, member_id: int):
         await ctx.send("Invalid member ID. You are not authorized to add users.")
 
 @bot.command(name="addchannel",description="add a channel along with its server's name")
-async def addchannel(ctx, username:str, server_name: str, channel_id: int, member_id: int):
+async def addchannel(ctx, username:str, server_name: str, channel_id: int, member_id: str):
     try:
         if member_id == VERIFIED_MEMBER_ID:
             try:
@@ -56,7 +55,7 @@ async def addchannel(ctx, username:str, server_name: str, channel_id: int, membe
         raise Exception(f"Error: {e}")
 
 @bot.command(name="hlp", description="description of all commands")
-async def help(ctx,member_id:int):
+async def help(ctx,member_id:str):
     try:
 
         if member_id==VERIFIED_MEMBER_ID:
@@ -66,11 +65,12 @@ async def help(ctx,member_id:int):
             await ctx.send("please enter a correct verification ID")
     except Exception as e:
         await ctx.send(e)
-#EVENTS
 
+
+#EVENT
 async def upload_video(channel_id: int, video_file: str) -> bool:
     try:
-        channel = await client.fetch_channel(channel_id)
+        channel = await bot.fetch_channel(channel_id)
         with open(video_file, 'rb') as file:
             await channel.send(file=discord.File(file))
         logger.info(f"{pre} Video '{video_file}' uploaded to {channel.mention}")
@@ -82,18 +82,20 @@ async def upload_video(channel_id: int, video_file: str) -> bool:
         logger.error(f"{pre} Error uploading video: {e}")
         return False
 
-
 async def is_connected():
     url = 'https://google.com'
     logger.info(f"{pre} Checking the connectivity of internet...")
     try:
-        requests.get(url)
-        logger.info(f"{pre} We are up and running")
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    logger.info(f"{pre} We are up and running")
+                else:
+                    logger.warning(f"{pre} Received non-200 status code: {response.status}")
     except Exception as e:
         logger.warning(f"{pre} We are not connected to internet... Trying again in 30 seconds")
         await asyncio.sleep(30)
         await is_connected()
-
 
 async def main():
     await update_account_records(query=None)
@@ -124,15 +126,20 @@ async def main():
         else:
             logger.info(f"{pre} No new post to check for account {account_name}")
 
-
 @bot.event
 async def on_ready():
     logger.info(f"{pre} We have logged in as {bot.user}")
     await is_connected()
-    while True:
-        await main()
-        logger.info(f"{pre} Waiting for 1 hour")
-        await asyncio.sleep(3600)
+    bot.loop.create_task(main_loop())
 
+async def main_loop():
+    while True:
+        try:
+            await main()
+            logger.info(f"{pre} Waiting for 1 hour")
+            await asyncio.sleep(3600)
+        except Exception as e:
+            logger.error(f"{pre} Error in main loop: {e}")
+            await asyncio.sleep(60) 
 
 bot.run(TOKEN)
